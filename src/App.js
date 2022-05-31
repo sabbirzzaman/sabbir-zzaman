54d99563f2a9583198cf5303c3a4dedbdcc101c7
sabbirzzaman <sztonmoy7@gmail.com>
2022-06-01 03:47:21 +0600

    loading animation added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import Footer from './Pages/Common/Footer/Footer';
 import Header from './Pages/Common/Header/Header';
 import Home from './Pages/Home/Home/Home';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Loading from './Pages/Common/Loading/Loading';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
+    if (loading){
+        return <Loading></Loading>
+    }
+
     return (
         <>
             <Header></Header>
